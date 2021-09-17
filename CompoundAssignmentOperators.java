42fedc21be8ccf63579e74ccd261e0773f8ff660
Ryan Kelley <kelleyr@duvalschools.org>
2021-09-17 00:00:25 -0400

    Create CompoundAssignmentOperators.java
    Template file.

---
+++ CompoundAssignmentOperators.java
@@ -0,0 +1,53 @@
+/*
+      .o.       ooooooooo.          .oooooo.    .oooooo..o       .o.                 
+     .888.      `888   `Y88.       d8P'  `Y8b  d8P'    `Y8      .888.                
+    .8"888.      888   .d88'      888          Y88bo.          .8"888.               
+   .8' `888.     888ooo88P'       888           `"Y8888o.     .8' `888.              
+  .88ooo8888.    888              888               `"Y88b   .88ooo8888.             
+ .8'     `888.   888              `88b    ooo  oo     .d8P  .8'     `888.            
+o88o     o8888o o888o              `Y8bood8P'  8""88888P'  o88o     o8888o                 
+                                                                                     
+ooooooooooooo   .oooooo.   ooooooooo.   ooooo   .oooooo.          .o            .o   
+8'   888   `8  d8P'  `Y8b  `888   `Y88. `888'  d8P'  `Y8b       o888          .d88   
+     888      888      888  888   .d88'  888  888                888        .d'888   
+     888      888      888  888ooo88P'   888  888                888      .d'  888   
+     888      888      888  888          888  888                888      88ooo888oo 
+     888      `88b    d88'  888          888  `88b    ooo        888  .o.      888   
+    o888o      `Y8bood8P'  o888o        o888o  `Y8bood8P'       o888o Y8P     o888o  
+
+Author Name, Date, Time, version 0.0
+                                                                            
+*/
+
+public class CompoundAssignmentOperators {
+
+    public static void main(String[] args) {
+        // Declare int variables named myInt0 and myInt1 and assign them any value.
+
+        // Declare double variables named myDouble0 and myDouble1 and assign them any
+        // value.
+
+        /*
+         * Write code demonstrating the use of the five compound operators using the
+         * variables declared above. Print those results on the screen. Make sure to
+         * avoid Divide by Zero exception.
+         */
+
+        /*
+         * Write code demonstrating the use of the the increment operator using the
+         * variables declared above. 1. Print the value of a variable on the screen. 2.
+         * Use the increment operator to change the value. 3. Print the value of that
+         * variable on the screen again. 4. Repeat steps 1-3.
+         * 
+         */
+
+        /*
+         * Write code demonstrating the use of the the deccrement operator using the
+         * variables declared above. 1. Print the value of a variable on the screen. 2.
+         * Use the decrement operator to change the value. 3. Print the value of that
+         * variable on the screen again. 4. Repeat steps 1-3.
+         * 
+         */
+
+    }
+}
